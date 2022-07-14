541efcca574b963964f8d270293837656335a2bc
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-07-14 17:58:30 -0400

    extending data model

--- model/setup.sql
+++ model/setup.sql
@@ -1,9 +1,32 @@
 -- Table: public.tasks
 
-DROP TABLE IF EXISTS public.tasks;
+CREATE TYPE task_status AS ENUM ('BACKLOG', 'DOING', 'DONE', 'DEPRIORITIZED', 'ARCHIVE');
 
-DROP TYPE task_status;
-CREATE TYPE task_status AS ENUM ('BACKLOG', 'DOING', 'DONE', 'DEPRIORITIZED');
+CREATE TYPE story_status AS ENUM ('BACKLOG', 'DOING', 'DONE', 'DEPRIORITIZED', 'ARCHIVE');
+
+CREATE TABLE IF NOT EXISTS public.sprints
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone,
+		title character varying(50) NOT NULL,
+		start_date date NOT NULL,
+		end_date date NOT NULL,
+		CONSTRAINT sprint_start_date_before_end_date CHECK (start_date < end_date)
+);
+
+CREATE TABLE IF NOT EXISTS public.stories
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone,
+		title character varying(50) NOT NULL,
+		description character varying(240),
+    status story_status DEFAULT 'BACKLOG'::story_status,
+		sprint_id uuid,
+		CONSTRAINT fk_sprint_id FOREIGN KEY(sprint_id) REFERENCES sprints(id),
+		CONSTRAINT title_not_empty CHECK (title <> '')
+);
 
 CREATE TABLE IF NOT EXISTS public.tasks
 (
@@ -13,8 +36,9 @@ CREATE TABLE IF NOT EXISTS public.tasks
 		title character varying(50) NOT NULL,
 		description character varying(240),
     status task_status DEFAULT 'BACKLOG'::task_status,
-		CONSTRAINT title_empty CHECK (title <> ''),
-		CONSTRAINT description_empty CHECK (description <> '')
+		story_id uuid,
+		CONSTRAINT fk_story_id FOREIGN KEY(story_id) REFERENCES stories(id),
+		CONSTRAINT title_not_empty CHECK (title <> '')
 );
 
 CREATE TABLE IF NOT EXISTS public.comments
@@ -25,15 +49,24 @@ CREATE TABLE IF NOT EXISTS public.comments
     updated_at timestamp without time zone,
 		text character varying(1000) NOT NULL,
     edited boolean NOT NULL DEFAULT false,
-		CONSTRAINT fk_task FOREIGN KEY(task_id) REFERENCES tasks(id),
-		CONSTRAINT text_empty CHECK (text <> '')
+		CONSTRAINT fk_task_id FOREIGN KEY(task_id) REFERENCES tasks(id),
+		CONSTRAINT text_not_empty CHECK (text <> '')
 );
 
+CREATE TABLE IF NOT EXISTS public.config
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+		key character varying(50) NOT NULL,
+		value character varying(1000) NOT NULL
+);
 
-INSERT INTO tasks (updated_at, title, description) VALUES
-(CURRENT_TIMESTAMP,	'Sample Task', 'Create the rest of the TODO app');
+INSERT INTO config (key, value) VALUES ('sprint_duration_seconds', EXTRACT(EPOCH FROM '2 weeks'::interval));
 
-TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.tasks
-    OWNER to postgres;
+-- INSERT INTO tasks (updated_at, title, description) VALUES
+-- (CURRENT_TIMESTAMP,	'Sample Task', 'Create the rest of the TODO app');
+
+-- TABLESPACE pg_default;
+
+-- ALTER TABLE IF EXISTS public.tasks
+--     OWNER to postgres;
