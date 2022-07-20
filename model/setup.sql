dbbf9835a14f75a08fbb871496284170a8527aec
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-07-20 01:27:03 +0000

    add grey background, more configs

--- model/setup.sql
+++ model/setup.sql
@@ -56,13 +56,19 @@ CREATE TABLE IF NOT EXISTS public.comments
 CREATE TABLE IF NOT EXISTS public.config
 (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    key character varying(50) NOT NULL,
+    key character varying(50) NOT NULL UNIQUE,
     value character varying(1000) NOT NULL
 );
 
 INSERT INTO config (key, value) VALUES
+	('server_name', 'TODO-APP-SERVER'),
 	('sprint_duration_seconds', EXTRACT(EPOCH FROM '2 weeks'::interval)),
-	('server_name', 'TODO-APP-SERVER');
+	('sprint_title_max_len', '150'),
+	('story_title_max_len', '150'),
+	('task_title_max_len', '150'),
+	('story_desc_max_len', '2000'),
+	('task_desc_max_len', '2000'),
+	('comment_max_len', '2000');
 
 
 INSERT INTO stories (updated_at, title, description, sprint_id) VALUES
