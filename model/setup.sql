6810fab7100972f37942a42a42f28988b18993a8
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-07-24 22:33:45 -0400

    add tags and tag assignments to data model

--- model/setup.sql
+++ model/setup.sql
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS public.sprints
     title character varying(150) NOT NULL,
     start_date date NOT NULL,
     end_date date NOT NULL,
+    edited boolean NOT NULL DEFAULT false,
     CONSTRAINT sprint_start_date_before_end_date CHECK (start_date < end_date)
 );
 
@@ -24,6 +25,7 @@ CREATE TABLE IF NOT EXISTS public.stories
     description character varying(2000),
     status story_status DEFAULT 'BACKLOG'::story_status,
     sprint_id uuid,
+    edited boolean NOT NULL DEFAULT false,
     CONSTRAINT fk_sprint_id FOREIGN KEY(sprint_id) REFERENCES sprints(id),
     CONSTRAINT title_not_empty CHECK (title <> '')
 );
@@ -37,8 +39,31 @@ CREATE TABLE IF NOT EXISTS public.tasks
     description character varying(2000),
     status task_status DEFAULT 'BACKLOG'::task_status,
     story_id uuid,
+    edited boolean NOT NULL DEFAULT false,
     CONSTRAINT fk_story_id FOREIGN KEY(story_id) REFERENCES stories(id),
     CONSTRAINT title_not_empty CHECK (title <> '')
+);
+
+CREATE TABLE IF NOT EXISTS public.tags
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone,
+    title character varying(30) NOT NULL,
+    description character varying(2000),
+    edited boolean NOT NULL DEFAULT false,
+		is_parent boolean NOT NULL DEFAULT false,
+    CONSTRAINT title_not_empty CHECK (title <> '')
+);
+
+CREATE TABLE IF NOT EXISTS public.tag_assignments
+(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		tag_id uuid NOT NULL,
+		story_id uuid NOT NULL,
+    CONSTRAINT fk_tag_id FOREIGN KEY(tag_id) REFERENCES tags(id),
+    CONSTRAINT fk_story_id FOREIGN KEY(story_id) REFERENCES stories(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.comments
@@ -77,9 +102,9 @@ INSERT INTO stories (updated_at, title, description, sprint_id) VALUES
 
 
 -- INSERT INTO tasks (updated_at, title, description) VALUES
--- (CURRENT_TIMESTAMP,  'Sample Task', 'Create the rest of the TODO app');
+-- (CURRENT_TIMESTAMP, 'Sample Task', 'Create the rest of the TODO app');
 
 -- TABLESPACE pg_default;
 
 -- ALTER TABLE IF EXISTS public.tasks
---     OWNER to postgres;
+--    OWNER to postgres;
