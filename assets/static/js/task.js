d292845c6aba8797b35805b0b69bb232002c18a6
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-10-30 21:32:33 +0000

    use modules for task view

--- assets/static/js/task.js
+++ assets/static/js/task.js
@@ -1,3 +1,4 @@
+import { getConfig, getCommentsByTaskId, getSprints, getStories, STATUSES, getTaskById, formatId, formatDate, NULL_STORY_IDENTIFIER } from './common.js';
 (async function () {
 	const path = window.location.pathname;
 	const taskIdFromPath = path.substring(path.lastIndexOf("/") + 1);
