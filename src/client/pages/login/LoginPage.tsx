df8b36a75b0ef1d2fc52d3cf9f25c4277f6606a5
Brendan Schlaman <brendan.schlaman@gmail.com>
2023-09-17 15:16:34 -0700

    add some styles to login screen

--- src/client/pages/login/LoginPage.tsx
+++ src/client/pages/login/LoginPage.tsx
@@ -16,12 +16,52 @@ const routes: { login: Route } = {
 
 export default function LoginPage() {
   return (
-    <>
-      <form action={routes.login.path} method={routes.login.method}>
-        <label htmlFor="pass">Enter Password:</label>
-        <input autoFocus type="password" required name="pass" />
-        <button>Login</button>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <form
+        style={{
+          borderRadius: "8px",
+          // TODO: consider using --color3 or making this --color3
+          boxShadow: "rgba(72, 135, 202, 0.8) 0 0 10px 3px",
+          fontSize: "3rem",
+          padding: "3rem",
+          transform: "translateY(-8rem)",
+        }}
+        action={routes.login.path}
+        method={routes.login.method}
+      >
+        <label htmlFor="pass">Enter Password</label>
+        <input
+          style={{
+            display: "block",
+            margin: "1rem 0",
+            padding: "0.8rem",
+            borderRadius: "8px",
+          }}
+          autoFocus
+          type="password"
+          required
+          name="pass"
+        />
+        <button
+          style={{
+            background: "rgb(72, 135, 202)",
+            color: "var(--transp-white)",
+            padding: "1rem 1.5rem",
+            borderRadius: "5px",
+            border: "none",
+            boxShadow: "0 0 6px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          Login
+        </button>
       </form>
-    </>
+    </div>
   );
 }
