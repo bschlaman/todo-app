495ac8b2746ed188f969d84ad5fdbe8eb360e037
Brendan Schlaman <brendan.schlaman@gmail.com>
2026-02-25 04:00:38 +0000

    use older version of story select items

--- src/client/components/story_select.tsx
+++ src/client/components/story_select.tsx
@@ -1,11 +1,17 @@
-import { MenuItem, Select } from "@mui/material";
+import {
+  ListSubheader,
+  MenuItem,
+  Select,
+  type SelectChangeEvent,
+} from "@mui/material";
 import React from "react";
 import { TagBadge } from "./tags";
-import type { Story, Tag } from "../ts/model/entities";
+import type { Sprint, Story, Tag } from "../ts/model/entities";
 import { isActive } from "../ts/model/status";
 import { NULL_STORY_IDENTIFIER } from "../ts/lib/common";
+import { type CSSObject } from "@emotion/react";
 
-export function renderStorySelectMenuItem2(
+export function renderStorySelectMenuItemOldVersion(
   story: Story,
   tagsById: Map<string, Tag>,
   assocTagIdsByStoryId: Map<string, string[]>,
@@ -31,6 +37,8 @@ export function renderStorySelectMenuItem2(
   );
 }
 
+// TODO (2026.02.24): something is wrong with this version;
+// it doesn't center properly, and not clear what it improves over renderStorySelectMenuItemOldVersion
 export function renderStorySelectMenuItem(
   story: Story,
   tagsById: Map<string, Tag>,
@@ -99,4 +107,167 @@ export function StorySelect({
       ))}
     </Select>
   );
+}
+
+export function StoryDropdown({
+  taskStoryId,
+  stories,
+  sprints,
+  tagsById,
+  assocTagIdsByStoryId,
+  handleTaskMetadataChange,
+}: {
+  taskStoryId: string;
+  stories: Story[];
+  sprints: Sprint[];
+  tagsById: Map<string, Tag>;
+  assocTagIdsByStoryId: Map<string, string[]>;
+  handleTaskMetadataChange: (e: SelectChangeEvent<string>) => void;
+}) {
+  // bucketize the stories by sprint
+  const sprintBuckets = new Map<string, Story[]>();
+  for (const story of stories) {
+    if (!sprintBuckets.has(story.sprint_id))
+      sprintBuckets.set(story.sprint_id, []);
+    sprintBuckets.get(story.sprint_id)?.push(story);
+  }
+
+  const sprintsToRender: Sprint[] = [];
+
+  // only render 3 additional sprints after the task is found
+  // to de-clutter the UI
+  let taskSprintFound = false;
+  let sprintsBeforeTask = 3;
+
+  sprints
+    .sort(
+      (sprint0, sprint1) =>
+        new Date(sprint1.start_date).getTime() -
+        new Date(sprint0.start_date).getTime(),
+    )
+    .forEach((sprint) => {
+      if (sprintsBeforeTask === 0) return;
+      if (taskSprintFound) sprintsBeforeTask--;
+      sprintBuckets.get(sprint.id)?.forEach((story) => {
+        if (story.id === taskStoryId) taskSprintFound = true;
+      });
+      sprintsToRender.push(sprint);
+    });
+
+  // If I don't perform this array length check,
+  // might try and render the select with a value which is not
+  // available yet
+  // TODO (2025.12.12) this theme thing is basically totally broken
+  return stories.length > 0 && sprints.length > 0 ? (
+    <Select
+      name="story_id"
+      value={taskStoryId}
+      onChange={handleTaskMetadataChange}
+      SelectDisplayProps={{
+        style: {
+          display: "flex",
+          alignItems: "center",
+        },
+      }}
+      // https://mui.com/system/getting-started/the-sx-prop/
+      // maybe I can remove the `as CSSObject` someday
+      sx={[
+        (theme) => theme.applyStyles("dark", { color: "white" }) as CSSObject,
+      ]}
+    >
+      <ListSubheader
+        key={NULL_STORY_IDENTIFIER}
+        sx={[
+          (theme) => ({
+            fontSize: "1.5rem",
+            backgroundColor: theme.palette.background.paper,
+            color: theme.palette.text.primary,
+          }),
+          (theme) =>
+            theme.applyStyles("dark", {
+              backgroundColor: "#374151",
+              color: "white",
+            }) as CSSObject,
+        ]}
+      >
+        {NULL_STORY_IDENTIFIER}
+      </ListSubheader>
+      <MenuItem
+        value={NULL_STORY_IDENTIFIER}
+        sx={[
+          (theme) => ({
+            marginLeft: "9rem",
+            borderBottom: `1px solid ${theme.palette.divider}`,
+            backgroundColor: theme.palette.background.paper,
+            color: theme.palette.text.primary,
+          }),
+          (theme) =>
+            theme.applyStyles("dark", {
+              borderBottom: "1px solid #4B5563",
+              backgroundColor: "#374151",
+              color: "white",
+            }) as CSSObject,
+        ]}
+      >
+        <strong>{NULL_STORY_IDENTIFIER}</strong>
+      </MenuItem>
+      {sprintsToRender.map((sprint) => {
+        // Explicitly checking undefined to make typescript happy
+        const stories = sprintBuckets.get(sprint.id);
+        if (stories === undefined) return [];
+
+        return [
+          <ListSubheader
+            key={sprint.id}
+            sx={[
+              (theme) => ({
+                fontSize: "1.5rem",
+                backgroundColor: theme.palette.background.paper,
+                color: theme.palette.text.primary,
+              }),
+              (theme) =>
+                theme.applyStyles("dark", {
+                  backgroundColor: "#374151",
+                  color: "white",
+                }) as CSSObject,
+            ]}
+          >
+            {sprint.title}
+          </ListSubheader>,
+          ...stories.map((story) => (
+            <MenuItem
+              key={story.id}
+              value={story.id}
+              disabled={!isActive(story)}
+              sx={[
+                (theme) => ({
+                  borderBottom: `1px solid ${theme.palette.divider}`,
+                  backgroundColor: theme.palette.background.paper,
+                  color: theme.palette.text.primary,
+                  "&.Mui-disabled": {
+                    color: theme.palette.text.disabled,
+                  },
+                }),
+                (theme) =>
+                  theme.applyStyles("dark", {
+                    borderBottom: "1px solid #4B5563",
+                    backgroundColor: "#374151",
+                    color: "white",
+                    "&.Mui-disabled": {
+                      color: "#9CA3AF",
+                    },
+                  }) as CSSObject,
+              ]}
+            >
+              {renderStorySelectMenuItemOldVersion(
+                story,
+                tagsById,
+                assocTagIdsByStoryId,
+              )}
+            </MenuItem>
+          )),
+        ];
+      })}
+    </Select>
+  ) : null;
 }
